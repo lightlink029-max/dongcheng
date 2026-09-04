c043d81cfcabd1563438152cd11b401ecb85ac00
lightlink029-max <lightlink029@gmail.com>
2026-09-04 13:05:49 +0800

    Release supplier sourcing fields 19.0.1.46.16

--- product_intelligence_hub/__manifest__.py
+++ product_intelligence_hub/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "产品智能中心",
-    "version": "19.0.1.46.15",
+    "version": "19.0.1.46.16",
     "summary": "采集、评分、审核并转化产品机会",
     "category": "Sales",
     "author": "LightLink",
@@ -17,6 +17,7 @@
         "views/product_intelligence_sourcing_views.xml",
         "views/product_intelligence_candidate_views.xml",
         "views/product_template_views.xml",
+        "views/res_partner_views.xml",
         "views/website_product_details_templates.xml",
         "views/res_config_settings_views.xml",
         "views/product_intelligence_menus.xml",
