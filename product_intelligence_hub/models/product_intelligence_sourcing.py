f25070929f43f9facc2cb261bb715b0169fa70c7
lightlink029-max <lightlink029@gmail.com>
2026-09-04 12:04:27 +0800

    Open YiwuGo homepage for image sourcing

--- product_intelligence_hub/models/product_intelligence_sourcing.py
+++ product_intelligence_hub/models/product_intelligence_sourcing.py
@@ -379,10 +379,10 @@ class ProductIntelligenceCandidateSourcing(models.Model):
             raise UserError(_("当前产品没有可用于找货的参考图片。"))
         if not self.sourcing_image_url:
             self.sourcing_image_url = image_url
-        image_query = quote_from_bytes(image_url.encode("utf-8"), safe="")
         url = (
-            "https://www.yiwugo.com/search/imgsearch.html?url=%s"
-            "&pih_candidate_id=%s&pih_search_mode=image" % (image_query, self.id)
+            "https://www.yiwugo.com/?"
+            "spm=d3d3Lnlpd3Vnby5jb20vc2VhcmNoL2ltZ3NlYXJjaC5odG1s"
+            "&pih_candidate_id=%s&pih_search_mode=image" % self.id
         )
         return {"type": "ir.actions.act_url", "url": url, "target": "new"}
 
