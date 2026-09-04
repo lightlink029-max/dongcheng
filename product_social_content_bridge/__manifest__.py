77b8995fd65b8fbcda31f9c29a91cf2f179a1f4c
lightlink029-max <lightlink029@gmail.com>
2026-09-04 19:11:45 +0800

    Add social content bridge __manifest__.py

---
+++ product_social_content_bridge/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "产品线社媒发布中心",
+    "version": "19.0.1.1.0",
+    "summary": "按产品线、市场和渠道批量生成并管理社媒内容",
+    "category": "Marketing/Social Marketing",
+    "author": "LightLink",
+    "license": "LGPL-3",
+    "depends": ["product_intelligence_hub", "social", "crm", "website_sale", "purchase", "stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/social_content_views.xml",
+        "views/crm_lead_views.xml",
+        "views/social_content_menus.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
